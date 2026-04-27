cd27f018fb4aeac218e3252b874a8f17a1f9d2ba
notahumanhand <76626091+notahumanhand@users.noreply.github.com>
2026-04-27 03:46:19 -0400

    Added user profile pictures

--- Core/User.cs
+++ Core/User.cs
@@ -4,6 +4,7 @@ namespace MythRPG.Core
 {
     public class User : IdentityUser
     {
+        public string? ProfilePictureUrl { get; set; }
         public List<Character> Characters { get; set; } = new List<Character>();
     }
 }
