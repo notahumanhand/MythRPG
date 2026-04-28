5a4eeedf95086f3dc9e4488acabf2a9015a0cd10
notahumanhand <76626091+notahumanhand@users.noreply.github.com>
2026-04-27 20:33:27 -0400

    Limited EFCore logging

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Identity;
 using HealthChecks.SqlServer;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.AddFilter("Microsoft.EntityFrameworkCore", LogLevel.Warning);
 builder.Configuration.AddEnvironmentVariables();
 
 builder.Services
