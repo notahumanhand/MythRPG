83dc2373efb1ba133c5acfa222aef8cfd4b11c3a
notahumanhand <76626091+notahumanhand@users.noreply.github.com>
2026-06-18 21:43:45 -0400

    Removed unused code, modified character list display format, added colour-coded borders to spells

--- Program.cs
+++ Program.cs
@@ -69,44 +69,6 @@ var app = builder.Build();
         app.UseHsts();
     }
 
-using (var scope = app.Services.CreateScope())
-{
-    var roleManager = scope.ServiceProvider
-        .GetRequiredService<RoleManager<IdentityRole>>();
-
-    if (!await roleManager.RoleExistsAsync("Spectator"))
-    {
-        await roleManager.CreateAsync(
-            new IdentityRole("Spectator"));
-    }
-}
-
-using (var scope = app.Services.CreateScope())
-{
-    var userManager =
-        scope.ServiceProvider
-            .GetRequiredService<UserManager<User>>();
-
-    var spectator =
-        await userManager.FindByNameAsync("Spectator");
-
-    if (spectator == null)
-    {
-        spectator = new User
-        {
-            UserName = "Spectator"
-        };
-
-        await userManager.CreateAsync(
-            spectator,
-            "Spectator123!");
-
-        await userManager.AddToRoleAsync(
-            spectator,
-            "Spectator");
-    }
-}
-
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
