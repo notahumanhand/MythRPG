aa5c3e47d222cb6ace149c2c6071a4b52023c3a0
notahumanhand <76626091+notahumanhand@users.noreply.github.com>
2026-06-21 18:47:52 -0400

    Implemented markdown support for descriptions

---
+++ Utilities/MarkdownHelper.cs
@@ -0,0 +1,22 @@
+﻿using Markdig;
+using Microsoft.AspNetCore.Components;
+
+namespace MythRPG.Utilities;
+
+public static class MarkdownHelper
+{
+    private static readonly MarkdownPipeline _pipeline =
+        new MarkdownPipelineBuilder()
+            .UseSoftlineBreakAsHardlineBreak()
+            .Build();
+
+    public static MarkupString ToMarkup(string? markdown)
+    {
+        if (string.IsNullOrWhiteSpace(markdown))
+            return new MarkupString(string.Empty);
+
+        string html = Markdown.ToHtml(markdown, _pipeline);
+
+        return new MarkupString(html);
+    }
+}
